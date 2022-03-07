fae3684d2fe3329773b9ba28da45351abfa523eb
baskiton <baskiton@mail.ru>
2022-03-08 01:52:06 +0700

    Peer fix

--- btorrent/peer.py
+++ btorrent/peer.py
@@ -1,19 +1,16 @@
-import hashlib
-
-
 class Peer:
     def __init__(self, ip, port: int, peer_id: bytes = None) -> None:
         self.ip = ip
         self.port = port
         self.peer_id = peer_id
         self.status = None
-        self.id = hashlib.sha256(str((self.ip, self.port)).encode()).digest()
+        self.__id = self.ip, self.port
 
     def __hash__(self):
-        return int.from_bytes(self.id, 'little')
+        return hash(self.__id)
 
     def __eq__(self, other: 'Peer') -> bool:
-        return self.id == other.id
+        return self.__id == other.__id
 
     def __repr__(self) -> str:
         return f'Peer({self.ip}:{self.port})'
